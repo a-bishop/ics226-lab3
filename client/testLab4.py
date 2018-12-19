73c8fefd81c203dde2b8516b8694350c8f234016
Andrew Bishop <andrew.bishop53@gmail.com>
2018-12-18 19:37:10 -0800

    deleted some test files

--- client/testLab4.py
+++
@@ -1,3 +0,0 @@
-from os import system
-
-system('python3 client.py localhost 5556 PUT test7.jpg & python3 client.py localhost 5556 PUT test.txt & python3 client.py localhost 5556 PUT test2.txt & python3 client.py localhost 5556 PUT test12.zip')
